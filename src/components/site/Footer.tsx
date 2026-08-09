5d21666ac87f6527b2de77c88a8d3e603dfc0f65
mohdfarhan7201 <mohd.farhan.7201@gmail.com>
2026-08-10 01:33:07 +0530

    Replaced jagged footer wave with a perfectly smooth mathematical cubic bezier wave

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -8,14 +8,13 @@ export function Footer() {
       {/* SVG Wave Separator */}
       <div className="absolute top-0 left-0 w-full overflow-hidden leading-0 transform -translate-y-[99%]">
         <svg 
-          className="relative block w-full h-[50px] sm:h-[100px]" 
-          data-name="Layer 1" 
+          className="relative block w-full h-[50px] sm:h-[80px] lg:h-[120px]" 
           xmlns="http://www.w3.org/2000/svg" 
-          viewBox="0 0 1200 120" 
+          viewBox="0 0 1440 120" 
           preserveAspectRatio="none"
         >
           <path 
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V120H0V95.8C59.71,118.08,130.83,119.7,194.2,107.5,237.49,99.23,279.28,78.5,321.39,56.44Z" 
+            d="M0,60 C480,120 960,0 1440,60 L1440,120 L0,120 Z" 
             className="fill-[#0d0d0d]"
           ></path>
         </svg>
